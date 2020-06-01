bf790f95d7c59ed538e0893a8b10d2a5a7ba983a
Thanaphon <top.thanaphon@gmail.com>
2020-06-01 15:51:50 +0700

    update

--- app.js
+++ app.js
@@ -6,31 +6,23 @@ const app = express()
 const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use
-
-app.get('/', (req, res) => {
-   res.send('ok')
-})
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
     res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token) {
+function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {9960e6fa24d0a193487a68283e1898d6}'
+        'Authorization': 'Bearer {xxxxxxx}'
     }
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
-            type: 'hey',
-            text: 'Hello man'
-        },
-        {
-            type: 'Ask me',
-            text: 'How are you?'
+            type: 'text',
+            text: msg
         }]
     })
     request.post({
