0048cde03ee8ff5b6d0fe5125127cb8a166812a3
Thanaphon <top.thanaphon@gmail.com>
2020-06-01 19:19:19 +0700

    fix

--- app.js
+++ app.js
@@ -70,9 +70,9 @@ server()
                 text: msg
             }]
         })
-        let body2 = {
+        let body2 = JSON.stringify({
             message:'Help me !, I could not answer'
-        }
+        })
         request.post({
             url: 'https://api.line.me/v2/bot/message/reply',
             headers: headers,
