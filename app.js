a318f073be65255f4cfecc5d758177daed5a963c
Thanaphon <top.thanaphon@gmail.com>
2020-06-01 16:37:10 +0700

    up

--- app.js
+++ app.js
@@ -9,15 +9,34 @@ server()
     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
    
     .post('/webhook', function (req, res) {
-        let replyToken = req.body.events[0].replyToken;
-        let msg = req.body.events[0].message.text;
-        
-        console.log(`Message token : ${ replyToken }`);
-        console.log(`Message from chat : ${ msg }`);
-
-        res.json({
-            status: 200,
-            message: `Webhook is working!`
-        });
+        let reply_token = req.body.events[0].replyToken
+        let msg = req.body.events[0].message.text
+        reply(reply_token, msg)
+        res.sendStatus(200)
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+    function reply(reply_token) {
+        let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {9960e6fa24d0a193487a68283e1898d6}'
+        }
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: 'Hello'
+            },
+            {
+                type: 'text',
+                text: 'How are you?'
+            }]
+        })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });
+    }
