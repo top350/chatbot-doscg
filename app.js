021e5988f4fe3cb97fa1f886d9894b9c12e5f776
Thanaphon <top.thanaphon@gmail.com>
2020-06-01 23:17:31 +0700

    s

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 9999;
 const request = require('request');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-
+const result;
 server()
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: false}))
@@ -25,8 +25,8 @@ server()
      .listen(PORT, () => console.log(`Listening on ${ PORT }`));
     
     function reply(reply_token, msg) {
-        
-        
+        var result = getData()
+        console.log(result)
         let headers = {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer {69GpTgBkddFvBR7hH0ghIUBmBs3zPQKSxbhzTy7x5RBoBGHxS7VJlTxI5wH7BybHu9yOJ3fS5Hh9pmGnT/dBVjXeqaJfRwb/r08p5SDQtCauxh4t7VygyxRZ6EMIBCMayzoqas0TBBt3V+P1xijEZgdB04t89/1O/w1cDnyilFU=}'
@@ -35,33 +35,24 @@ server()
             'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization': 'Bearer BV9HXH2u3ECb5rH5Y9NwxM0UqtN2QO2ME5jaV7Un0XY'
         }
-      
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: msg
+            }]
+        })
         let body2 ={
             message:'Help me! I could not answer'
         }
-        axios.get('covid19.th-stat.com/api/open/today')
-  .then(function (response) {
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: response.data
-        }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
-  
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });
+      
         request.post({
             url: 'https://notify-api.line.me/api/notify',
             headers: headers2,
